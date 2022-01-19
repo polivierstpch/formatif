83eba9af0bda84b930f22dcdbe761801e1be6e50
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-19 18:31:21 -0500

    webapi -- ajout des méthodes d'action Get Post Delete pour les produits.

---
+++ src/Web2.API/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using Web2.API.Models;
+using System.Linq;
+
+namespace Web2.API.Controllers
+{   
+    [ApiController]
+    [Route("products")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly List<Product> _products = new() { new Product { Id = 1, Name = "Patate" } };
+
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return _products;
+        }
+
+        public ActionResult<Product> Get(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product is null)
+                return NotFound();
+
+            return product;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            var productExists = _products.Any(p => p.Id == product.Id);
+
+            if (productExists)
+                return BadRequest();
+
+            product.Id = _products.Count + 1;
+            _products.Add(product);
+
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var productToDelete = _products.FirstOrDefault(p => p.Id == id);
+
+            if (productToDelete is null)
+                return NotFound();
+
+            _products.Remove(productToDelete);
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/Web2.API/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Web2.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
