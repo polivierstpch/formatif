90ecb6316abf028d432291ea4b4027d171678f35
Pier-Olivier St-Pierre-Chouinard <pier.olivier.stpch@gmail.com>
2022-01-19 23:19:58 -0500

    webapi -- ajout de la configuration et documentation API avec Swagger

--- src/Web2.API/Controllers/ProductsController.cs
+++ src/Web2.API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 using Web2.API.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace Web2.API.Controllers
 {   
@@ -12,12 +13,26 @@ namespace Web2.API.Controllers
     public class ProductsController : ControllerBase
     {
         private static readonly List<Product> _products = new() { new Product { Id = 1, Name = "Patate" } };
-
+        
+        /// <summary>
+        /// Retourne une liste de produits.
+        /// </summary>
+        /// <returns>Une liste de produits (peut être vide)</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<Product>> Get()
         {
             return _products;
         }
-
+        
+        /// <summary>
+        /// Retourne le produit avec l'id passé dans l'URI
+        /// </summary>
+        /// <param name="id">Identifiant unique du produit</param>
+        /// <returns>Le produit avec l'id ou bien une réponse 404 NotFound</returns>
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<Product> Get(int id)
         {
             var product = _products.FirstOrDefault(p => p.Id == id);
@@ -27,8 +42,15 @@ namespace Web2.API.Controllers
 
             return product;
         }
-
+        
+        /// <summary>
+        /// Ajoute un produit à la liste, sauf s'il y a déjà un produit avec le même id.
+        /// </summary>
+        /// <param name="product">Le produit à ajouter</param>
+        /// <returns>Une réponse HTTP avec l'URI pour obtenir le produit, ainsi qu'une copie du produit dans le corps de la réponse.</returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(Product product)
         {
             var productExists = _products.Any(p => p.Id == product.Id);
@@ -41,8 +63,15 @@ namespace Web2.API.Controllers
 
             return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
         }
-
+        
+        /// <summary>
+        /// Supprime un produit de la liste selon l'id passé dans l'URI.
+        /// </summary>
+        /// <param name="id">Id du produit à supprimer</param>
+        /// <returns>Une réponse HTTP NoContent si la suppression est effectuée, sinon une réponse 404 NotFound</returns>
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Delete(int id)
         {
             var productToDelete = _products.FirstOrDefault(p => p.Id == id);
--- src/Web2.API/Startup.cs
+++ src/Web2.API/Startup.cs
@@ -1,7 +1,11 @@
+using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Web2.API
 {
@@ -12,6 +16,24 @@ namespace Web2.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Products API",
+                    Description = "An API to manage Product items.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Pier-Olivier St-Pierre-Chouinard",
+                        Email = "pier.olivier.stpch@gmail.com"
+                    }
+                });
+
+                var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -20,6 +42,11 @@ namespace Web2.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
             }
 
             app.UseRouting();
